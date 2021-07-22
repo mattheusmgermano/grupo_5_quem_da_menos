eaf8c02450f462a7b416144af116a694d91cf7f6
Natália Dal Pizzol <nataliadpizzol@gmail.com>
2021-07-21 23:02:18 -0300

    Adicionando item da lista de categoria direto na lista de compras do usuário

--- quem_da_menos/lib/screens/categoryItems.dart
+++ quem_da_menos/lib/screens/categoryItems.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quem_da_menos/models/Category.dart';
+import 'package:quem_da_menos/staticData/marketList.dart';
 import 'package:quem_da_menos/staticData/productList.dart';
+import 'package:quem_da_menos/staticData/userProdList.dart';
 
 class CategoryScreen extends StatelessWidget {
   final Category category;
@@ -16,6 +18,7 @@ class CategoryScreen extends StatelessWidget {
         child: GridView.count(
           primary: false,
           padding: EdgeInsets.all(10.0),
+          childAspectRatio: 0.90,
           crossAxisSpacing: 10.0,
           mainAxisSpacing: 5.0,
           crossAxisCount: 2,
@@ -25,23 +28,54 @@ class CategoryScreen extends StatelessWidget {
             return GestureDetector(
                 child: Card(
                   color: Colors.white,
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    padding: EdgeInsets.all(5.0),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(product.imagem),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,  // add this
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(8.0),
+                          topRight: Radius.circular(8.0),
+                        ),
+                        child: Image.network(
+                            product.imagem,
+                            // width: 300,
+                            height: 100,
+                            fit:BoxFit.fill
+                        ),
                       ),
-                    ),
+                      ListTile(
+                        title: Text(product.name),
+                        subtitle: findLowestPrice(product.id),
+                      ),
+                    ],
                   ),
                 ),
                 onTap: () {
-                  print("hii");
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("${product.name} Adicionado na Lista"),
+                  ));
+                  userProductList.add({"title": product.name, "ok": true});
                 });
           }).toList(),
         ),
       ),
     );
   }
+}
+
+Widget findLowestPrice(int productId) {
+  // Filters markets that have the product in hand
+  var lowestPriceMarket = marketList
+    .where((market) => market.produtos.any((produto) => produto.id == productId)).toList();
+
+  // Sorts markets by lowest product value
+  lowestPriceMarket.sort((a, b) => a.produtos.firstWhere((prod) => prod.id == productId).price < b.produtos.firstWhere((prod) => prod.id == productId).price ? 0 : 1);
+  var bestMarket = lowestPriceMarket[0];
+
+  if (lowestPriceMarket.isEmpty){
+    return Text("Produto não disponível");
+  }
+
+
+  return Text("R\$${bestMarket.produtos.firstWhere((prod) => prod.id == productId).price} em ${bestMarket.name}");
 }
---
+++ quem_da_menos/lib/staticData/marketList.dart
@@ -0,0 +1,7 @@
+
+import 'package:quem_da_menos/models/supermercado_model.dart';
+
+List<Supermercado> marketList = [
+  Zaffari(),
+  Carrefour()
+];
